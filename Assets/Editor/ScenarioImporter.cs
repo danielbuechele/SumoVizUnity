b0989a3ec0b9a376643362c8095b9b0aad5e373e
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2016-02-29 23:41:03 +0100

    add ScenarioImporter + RuntimeInitializer
    they set the structure for the workflow of importing the scenario at
    editing time instead of runtime as previously

---
+++ Assets/Editor/ScenarioImporter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Collections;
+
+public class ScenarioImporter : MonoBehaviour {
+
+	[MenuItem("Assets/Import accu:rate output")]
+	
+	static void importAccurateOutput() {
+		EditorApplication.SaveCurrentSceneIfUserWantsTo();
+
+		string currentSceneName = Path.GetFileNameWithoutExtension(EditorApplication.currentScene);
+		var continueOk = true;
+		if (currentSceneName == "Base") 
+			continueOk = !EditorUtility.DisplayDialog("duplicate scene", "It is recommend that you first duplicate the Scene (select it in the Scenes folder and use Edit > Duplicate), rename it optionally and doubleclick the new scene.", "ok, let me duplicate", "continue");
+
+		if (continueOk) {
+			RuntimeInitializer runtimeInitializer = GameObject.Find("RuntimeInitializer").GetComponent<RuntimeInitializer>();
+
+			runtimeInitializer.geometryLoader = GameObject.Find("GeometryLoader").GetComponent<GeometryLoader>();
+			runtimeInitializer.geometryLoader.setTheme (new NatureThemingMode ());
+
+			var path = EditorUtility.OpenFilePanel ("", Application.dataPath + "/Data", "xml");
+
+			/*
+			FileLoaderXML fl = new FileLoaderXML ();
+			fl.loadFileByPath(Path.GetFileName(path));
+			fl.buildGeometry();
+			runtimeInitializer.trajectoryLines = fileLoader.loadTrajectoryLines();
+			*/
+		}
+	}
+
+}
---
+++ Assets/Scripts/RuntimeInitializer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RuntimeInitializer : MonoBehaviour {
+
+	public GeometryLoader geometryLoader;
+	public List<string> trajectoryLines; 
+
+	void Start () {
+		//FileLoaderXML runtimeFileLoader = new FileLoaderXML();
+		//runtimeFileLoader.loadTrajectories (trajectoryLines);
+	}
+
+}
