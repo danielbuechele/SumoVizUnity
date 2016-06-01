f592104d323a0c9787ecb55adb002503e4f9ca16
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2016-06-01 22:01:33 +0200

    resurrect FileChooser
    rename the old one to FileChooser_old

---
+++ Assets/Scripts/FileChooser.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FileChooser : MonoBehaviour {
+
+	FileBrowser fb = new FileBrowser();
+
+	void Start () {
+		//fb.extensionsAllowed = new[] {".crowdit", ".xml"};
+	}
+
+	void OnGUI(){	
+		if (fb.draw()) {
+			if (fb.outputFile == null){
+				Debug.Log("Choosing file was cancelled");
+				Application.Quit();
+			} else {
+				Debug.Log("Output File = \"" + fb.outputFile.ToString() + "\"");
+
+				//fl.loadXMLFile(fb.outputFile.FullName);
+
+				Destroy (this);
+			}
+		}
+	}
+	
+	void Update () {}
+}
