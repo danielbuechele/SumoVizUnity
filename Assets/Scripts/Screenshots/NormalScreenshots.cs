488893ccbb8a404a05904dd39ce85e311d0da541
Benjamin Aaron Degenhart <benjamin.degenhart@gmail.com>
2016-06-15 13:45:05 +0200

    WIP screenrecorder
    attach script to camera for onPostRender to be called

--- Assets/Scripts/Screenshots/NormalScreenshots.cs
+++ Assets/Scripts/Screenshots/NormalScreenshots.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class NormalScreenshots : MonoBehaviour {
 
@@ -10,15 +11,33 @@ public class NormalScreenshots : MonoBehaviour {
 	private int count = 0;
 
 
+
 	void Start () {
 		pc = GameObject.Find("PlaybackControl").GetComponent<PlaybackControl>();
 		Time.captureFramerate = fps;
+
+
+		Screenrecorder.init ();
 	}
 	
-	void Update () {
+	void OnPostRender () {
 		if (pc.inFirstRound ()) {
-			Application.CaptureScreenshot ("Screenshots/screenshot" + (count ++) + ".png", superSizeFactor);
-			Debug.Log ("screenshot #" + count + " taken");
+			//Application.CaptureScreenshot ("Screenshots/screenshot" + (count ++) + ".png", superSizeFactor);
+
+			Texture2D screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false); // via http://answers.unity3d.com/answers/1190178/view.html
+			screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+			screenshot.Apply();
+
+			byte[] bytes = screenshot.EncodeToPNG();
+
+			//Screenrecorder.writeImg (bytes);
+
+			Object.Destroy(screenshot);
+
+			//File.WriteAllBytes(Application.dataPath + "/test.png", bytes);
+			//File.WriteAllBytes("Screenshots/screenshot" + (count ++) + ".png", bytes);
+
+			UnityEngine.Debug.Log ("screenshot #" + count + " taken");
 		}
 	}
 
---
+++ Assets/Scripts/Screenshots/Screenrecorder.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+
+
+public static class Screenrecorder {
+
+	public static Process process;
+	public static BinaryWriter writer;
+
+
+	public static void init() {
+		process = new Process ();
+
+
+		var absoluteFfmpegExeLoc = Application.dataPath + "/Plugins/other/ffmpeg/bin/ffmpeg.exe";
+
+
+		var relativeScreenshotFileGenericLoc = "pipe:.png";// "Screenshots\\screenshot%d.png";
+		var relativeOutFileLoc = "out.mp4";
+		var ffmpegCommand = "-f image2pipe -i " + relativeScreenshotFileGenericLoc + " -vf scale=trunc(iw/2)*2:trunc(ih/2)*2 -r 25 -c:v libx264 -pix_fmt yuv420p -crf 18 " + relativeOutFileLoc; // keep same resolution code from http://stackoverflow.com/a/20848224
+
+		process.StartInfo.UseShellExecute = false;
+		process.StartInfo.RedirectStandardInput = true;
+		process.StartInfo.RedirectStandardOutput = true;
+		process.StartInfo.FileName = absoluteFfmpegExeLoc;
+		//process.StartInfo.Arguments = ffmpegCommand;
+
+	
+
+		process.Start ();
+		writer = new BinaryWriter(process.StandardInput.BaseStream);
+
+
+		//process.StandardInput.WriteLine ("ffmpeg");
+		//process.WaitForExit ();
+	}
+
+	public static void writeImg(byte[] img) {
+		writer.Write (img);
+		//process.StandardInput.BaseStream.Write (img, 0, 0);
+		//System.Drawing.Image img;
+		//img.Save(writer.BaseStream, System.Drawing.Imaging.ImageFormat.Bmp);
+	}
+
+	public static void close() {
+		process.StandardInput.Close ();
+	}
+
+}
