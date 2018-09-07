c75ca52a7daae9b68cd94e3d65f45c28b9301357
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2018-09-07 18:49:19 +0200

    load pedestrians from all floors

--- Assets/Scripts/FileLoader/TrajectoryLoader.cs
+++ Assets/Scripts/FileLoader/TrajectoryLoader.cs
@@ -14,47 +14,49 @@ public class TrajectoryLoader {
         XmlDocument simXmlDoc = new XmlDocument();
         simXmlDoc.LoadXml(utils.loadFileIntoEditor(simXmlFilePath));
         XmlNode output = simXmlDoc.SelectSingleNode("//output");
-        string trajFilePath = "";
-        foreach (XmlElement floorCsvAtEl in output.SelectNodes("floor")) {
-            trajFilePath = Path.Combine(outFolder, floorCsvAtEl.GetAttribute("csvAt")); // TODO multiple floors
-        }
-
-        //if (trajFilePath.EndsWith(".gz")) // TODO
 
         PedestrianLoader pl = GameObject.Find("PedestrianLoader").GetComponent<PedestrianLoader>();
-        decimal timeSubtract = 0;
 
-        // via https://stackoverflow.com/a/29372751
-        using (FileStream fs = File.OpenRead(trajFilePath))
-        using (GZipStream zip = new GZipStream(fs, CompressionMode.Decompress, true))
-        using (StreamReader reader = new StreamReader(zip)) {
-        //while (!unzip.EndOfStream) {}
-		//using (reader) { // fs: a bit complicated, but this should cope even with totally inconsistent line endings
-			string line = reader.ReadLine(); // skip the 1st line = header
-			line = reader.ReadLine(); // 2nd line
+        foreach (XmlElement floorCsvAtEl in output.SelectNodes("floor")) {
+            string trajFilePath = Path.Combine(outFolder, floorCsvAtEl.GetAttribute("csvAt"));
+            //if (trajFilePath.EndsWith(".gz")) // TODO
 
-			if (forceStartAtZero && !timeSubstractTaken) {
-				decimal.TryParse(line.Split(',')[0], out timeSubtract);
-				timeSubstractTaken = true;
-			}
+            decimal timeSubtract = 0;
 
-			while(line != null) {
-				string[] values = line.Split(',');
-				if (values.Length >= 4) {
-					decimal time;
-					int id;
-					float x, y, z;
-					decimal.TryParse(values[0], out time);
-					int.TryParse(values[1], out id);
-					float.TryParse(values[2], out x);
-					float.TryParse(values[3], out y);
-                    float.TryParse(values[4], out z);
-                    pl.addPedestrianPosition(new PedestrianPosition(id, time - timeSubtract, x, y, z));
-				}
-				line = reader.ReadLine ();
-			}
-			reader.Close ();
-		}
+            // via https://stackoverflow.com/a/29372751
+            using (FileStream fs = File.OpenRead(trajFilePath))
+            using (GZipStream zip = new GZipStream(fs, CompressionMode.Decompress, true))
+            using (StreamReader reader = new StreamReader(zip)) {
+                //while (!unzip.EndOfStream) {}
+                //using (reader) { // fs: a bit complicated, but this should cope even with totally inconsistent line endings
+                string line = reader.ReadLine(); // skip the 1st line = header
+                line = reader.ReadLine(); // 2nd line
+
+                if (forceStartAtZero && !timeSubstractTaken) {
+                    decimal.TryParse(line.Split(',')[0], out timeSubtract);
+                    timeSubstractTaken = true;
+                }
+
+                while (line != null) {
+                    string[] values = line.Split(',');
+                    if (values.Length >= 4) {
+                        decimal time;
+                        int id;
+                        float x, y, z;
+                        decimal.TryParse(values[0], out time);
+                        int.TryParse(values[1], out id);
+                        float.TryParse(values[2], out x);
+                        float.TryParse(values[3], out y);
+                        float.TryParse(values[4], out z);
+                        pl.addPedestrianPosition(new PedestrianPosition(id, time - timeSubtract, x, y, z));
+                    }
+                    line = reader.ReadLine();
+                }
+                reader.Close();
+            }
+
+        }
+
 		//TODO find mechanism to not show peds (for quick camera-tour dev)
 		//pl.createPedestrians ();
 		pl.init ();
